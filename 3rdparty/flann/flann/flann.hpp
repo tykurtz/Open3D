d8f1b5ebaba120460538425d44cb200cdf024953
Tyler <tyler@vicarious.com>
2020-02-06 12:33:07 -0800

    Got open3d building using 3rdparty flann

--- 3rdparty/flann/flann/flann.hpp
+++ 3rdparty/flann/flann/flann.hpp
@@ -27,7 +27,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *************************************************************************/
-aoisjdfoiasjdfoiajdsoif
 
 #ifndef FLANN_HPP_
 #define FLANN_HPP_
